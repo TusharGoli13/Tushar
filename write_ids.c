3d36c5c267fe2e9bcead1ed0b8025132e0d31450
TusharGoli13 <126295025+TusharGoli13@users.noreply.github.com>
2024-09-13 17:49:36 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "tvg5391@psu.edu";
+    char STUDENT_ID[] = "927428633";
 
     return 0 ;
 }
